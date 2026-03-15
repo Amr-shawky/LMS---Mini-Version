5efebc94230bcb8f5b10d5798f0b76c854aef1f2
Ahmed Fathy <ahmedfathy11876@gmail.com>
2026-03-15 20:59:48 +0200

    let EnrollmentInternCommand.cs return EnrollmentResultDto instead of just return the bool
    Signed-off-by: Ahmed Fathy <ahmedfathy11876@gmail.com>

--- Features/Enrollments/Commands/EnrollmentIntern/EnrollmentInternCommand.cs
+++ Features/Enrollments/Commands/EnrollmentIntern/EnrollmentInternCommand.cs
@@ -1,6 +1,6 @@
 using LMS___Mini_Version.Mediators;
 using MediatR;
 
-namespace LMS___Mini_Version.Features.Enrollments.Commands.EnrollmentInternCommand;
+namespace LMS___Mini_Version.Features.Enrollments.Commands.EnrollmentIntern;
 
 public record EnrollmentInternCommand(int InternId,int TrackId) : IRequest<EnrollmentResultDto>;
