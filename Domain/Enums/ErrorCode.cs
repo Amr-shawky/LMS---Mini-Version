147126c2e62f955e096dd7c318eb5358f0091d15
3OmarReda <omarredaelkholy@gmail.com>
2026-03-23 16:41:15 +0200

    Add ErrorCode enum and ResultRequest<T> result wrapper
    Introduced ErrorCode enum in Domain/Enums with NoError value.
    Added ResultRequest<T> record to standardize result handling,
    including static methods for success and failure cases.

---
+++ Domain/Enums/ErrorCode.cs
@@ -0,0 +1,7 @@
+﻿namespace LMS___Mini_Version.Domain.Enums
+{
+    public enum ErrorCode
+    {
+    NoError = 0,
+    }
+}
---
+++ Result/ResultRequest.cs
@@ -0,0 +1,12 @@
+﻿using LMS___Mini_Version.Domain.Enums;
+
+namespace LMS___Mini_Version.Result
+{
+    public record ResultRequest<T>(T Data,bool IsSuccess,ErrorCode error)
+    {
+     public static ResultRequest<T> Success(T data) => new ResultRequest<T>(data, true,ErrorCode.NoError);
+     public static ResultRequest<T> Failure(ErrorCode error) => new ResultRequest<T>(default(T)!, false, error);
+
+     
+    }
+}
