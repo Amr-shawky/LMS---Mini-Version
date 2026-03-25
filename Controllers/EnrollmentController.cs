a2a53a4d346f3aa888d9b93c9da6529375ca6b72
asalaehab824@gmail.com <asalaehab824@gmail.com>
2026-03-25 12:06:42 +0200

    controllerPayment

---
+++ Controllers/EnrollmentController.cs
@@ -0,0 +1,26 @@
+﻿using LMS___Mini_Version.CQRS.Enrollments.Orchestrator;
+using LMS___Mini_Version.ViewModels.PaymentViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS___Mini_Version.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnrollmentController:ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public EnrollmentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StageNewEnrollmet(StageEnrollmentVM vM)
+        {
+           var EnrollmentResult=await _mediator.Send(new EnrollInternOrchestratorRequest(vM.InternId, vM.PaymentMethod));
+            // Return just success message
+            return  Ok(new { message = "Enrollment successful" });
+        }
+    }
+}
---
+++ ViewModels/PaymentViewModels/StageEnrollmentVM.cs
@@ -0,0 +1,14 @@
+﻿using LMS___Mini_Version.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS___Mini_Version.ViewModels.PaymentViewModels
+{
+    public class StageEnrollmentVM
+    {
+        [Required]
+        public int InternId { get; set; }
+
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; } = PaymentMethod.Cash;
+    }
+}
