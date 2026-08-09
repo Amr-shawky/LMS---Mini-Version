7408c3b4ee574e754448b939605af1658b09c19c
refaatbasant2030 <refaatbasant2030@gmail.com>
2026-08-10 02:06:43 +0300

    add MediatR lib and redister it in Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using LMS___Mini_Version.Mediators;
 using LMS___Mini_Version.Persistence;
 using LMS___Mini_Version.Services.Implementations;
 using LMS___Mini_Version.Services.Interfaces;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace LMS___Mini_Version
@@ -43,6 +44,7 @@ namespace LMS___Mini_Version
             builder.Services.AddScoped<EnrollInternMediator>();
             builder.Services.AddScoped<CancelEnrollmentMediator>();
             builder.Services.AddScoped<TransferEnrollmentMediator>();
+            builder.Services.AddMediatR(typeof(Program));
 
             var app = builder.Build();
 
