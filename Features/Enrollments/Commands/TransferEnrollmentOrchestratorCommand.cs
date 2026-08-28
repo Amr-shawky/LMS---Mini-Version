55bb966ee632145a49608cfbcdf7b19c789c25ad
Khader Mohamed <khaderelhabal923@gmail.com>
2026-08-28 20:19:29 +0300

    feat(enrollments): transfer enrollment orchestrator
    Add/update orchestrator command and handler for transferring enrollments.
    
    Includes TransferRequest view model changes supporting this flow.

---
+++ Features/Enrollments/Commands/TransferEnrollmentOrchestratorCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace LMS___Mini_Version.Features.Enrollments.Commands;
+
+public record TransferEnrollmentOrchestratorCommand(int EnrollmentId, int NewTrackId) : IRequest<bool>;
---
+++ ViewModels/Enrollment/TransferRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace LMS___Mini_Version.ViewModels.Enrollment;
+
+public class TransferRequest
+{
+    public int NewTrackId { get; set; }
+}
