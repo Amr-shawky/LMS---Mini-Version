73b3052d02ec17f6c20d38aa82dde4d479bf8841
Khader Mohamed <khaderelhabal923@gmail.com>
2026-08-28 20:17:43 +0300

    feat(enrollments): adjust payment amount command/handler
    Add/update command and handler for adjusting an enrollment's payment amount.

--- Features/Enrollments/Commands/CancelEnrollmentCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-
-namespace LMS___Mini_Version.Features.Enrollments.Commands;
-
-public record CancelEnrollmentCommand(int Id) : IRequest<bool>;
-
---
+++ Features/Enrollments/Handlers/AdjustPaymentAmountCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using LMS___Mini_Version.Domain.Repositories;
+using LMS___Mini_Version.Domain.Entities;
+using LMS___Mini_Version.Features.Enrollments.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS___Mini_Version.Features.Enrollments.Handlers;
+
+public class AdjustPaymentAmountCommandHandler
+    (IGeneralRepository<Payment> _paymentrepo):
+    IRequestHandler<AdjustPaymentAmountCommand, Unit>
+{
+
+    public async Task<Unit> Handle(AdjustPaymentAmountCommand request, CancellationToken cancellationToken)
+    {
+        var payment = await _paymentrepo.GetTable()
+            .FirstOrDefaultAsync(p => p.EnrollmentId == request.EnrollmentId, cancellationToken);
+
+        if (payment != null)
+        {
+            payment.Amount = request.NewAmount;
+        }
+        
+        return Unit.Value;
+    }
+}
